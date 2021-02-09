af6dd221cca3b59f80fed4733257a28323abc196
LuisPa <diganluispa@gmail.com>
2021-02-09 09:07:49 -0600

    refactor: Now we made parallel requests

--- src/verifyGuides.js
+++ src/verifyGuides.js
@@ -1,19 +1,25 @@
-const axios = require('axios');
-const getUrl = require('./getUrl');
+const axios = require("axios");
+const getUrl = require("./getUrl");
 const guidesObj = {
-	ale: '1W211083535',
-	mochy: '1W211084034'
+  ale: "1W211083535",
+  mochy: "1W211084034",
 };
 async function verifyGuides() {
-	try {
-		for (const guide of Object.keys(guidesObj)) {
-			const response = await axios.get(getUrl(guidesObj[guide]));
-			const status = response.data.tracking.datosGuia.PODStatusDes;
-			console.log(guide, guidesObj[guide], status);
-		}
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    const responses = await Promise.all(
+      Object.keys(guidesObj).map((guide) => axios.get(getUrl(guidesObj[guide])))
+    );
+
+    responses.map((response) => {
+      const { datosGuia } = response.data.tracking;
+      const status = datosGuia.PODStatusDes;
+      const nombre = datosGuia.DestinatarioNombre;
+      const numeroGuia = datosGuia.NumeroGuia;
+      console.log(nombre, " - ", numeroGuia, " - ", status);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 verifyGuides();
 
